3bd4b6b6615a8ee706afead7881155bca9e029d0
Angela <angelacr@ymail.com>
2021-04-04 20:36:21 -0500

    setting up classes in lib folder

---
+++ app.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const engineer = require("./lib/engineer");
+const manager = require("./lib/manager");
+const intern = require("./lib/intern");
